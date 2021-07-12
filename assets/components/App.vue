eed683c64ad1a9d78bedf8241c5caabf85d47f8f
unknown <stowwel@gmail.com>
2021-07-13 01:48:59 +0200

    adding Background switcher

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -19,12 +19,40 @@ export default {
   },
   data () { 
     return {
+      imageIndex: 0,
+      images: [
+        'AdobeStock_91717852.jpeg',
+        'AdobeStock_247520870.jpeg',
+        'AdobeStock_90488874.jpeg',
+        /*
+        'AdobeStock_154437497.jpeg',
+        'AdobeStock_94538497.jpeg',
+        'AdobeStock_103545648.jpeg',
+        'AdobeStock_277812854.jpeg',
+        */
+      ]
     }
   },
+  mounted () {
+    this.switchBackground();
+  },
   created () {
+    this.preloadImages();
   },
 
-    methods: {
+    methods: {    
+      preloadImages() {
+        this.images.forEach(url => new Image().src = url);
+      },
+      switchBackground() {
+        $('body').css('background-image', 'url('+ this.images[this.imageIndex]+')');
+        // now 'this' is referencing the Vue object and not the 'setTimeout' scope
+        setTimeout(() => this.switchBackground(), 5000);
+        this.imageIndex++;
+        if (this.imageIndex==this.images.length) {
+          this.imageIndex = 0;
+        }
+    }
   }
 };
 </script>
